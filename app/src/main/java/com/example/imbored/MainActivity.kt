0da9e79beb8bdbcd88c6b53d210299420b6cd779
Lee Waggoner <lwaggoner@roadrunner.com>
2023-09-08 11:31:53 +0800

    Added logo icons and animated splash screen

--- app/src/main/java/com/example/imbored/MainActivity.kt
+++ app/src/main/java/com/example/imbored/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.imbored
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,12 +10,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.imbored.ui.theme.ImBoredTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { splashScreenView ->
+                splashScreenView.remove()
+            }
+        }
+
         setContent {
             ImBoredTheme {
                 // A surface container using the 'background' color from the theme
