960e71d908a61e04ddc6780e35abf8acc0579744
Lee Waggoner <lwaggoner@roadrunner.com>
2023-10-01 11:12:18 +0800

    Fixed progress indicator

--- app/src/main/java/com/wreckingball/imbored/ui/activity/DisplayActivity.kt
+++ app/src/main/java/com/wreckingball/imbored/ui/activity/DisplayActivity.kt
@@ -1,6 +1,10 @@
 package com.wreckingball.imbored.ui.activity
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
@@ -8,6 +12,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -67,15 +72,26 @@ private fun DisplayActivityContent(
             ActivityImage(imageData = imageData)
         }
 
-        if(state.isLoading) {
-            CircularProgressIndicator()
-        }
-
         if (state.errorMessage.isNotEmpty()) {
             BoredErrorAlert(
                 message = state.errorMessage,
                 onDismissAlert = onDismissAlert
             )
+        }
+    }
+
+    if (state.isLoading) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() },
+                    onClick = { },
+                )
+        ) {
+            CircularProgressIndicator()
         }
     }
 }
